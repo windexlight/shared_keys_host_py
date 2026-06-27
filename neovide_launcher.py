c41540739b06fbd7475238e796bbe9c35bf1c9b9
windexlight <windexlight@gmail.com>
2026-06-27 15:37:00 -0700

    Make neovide_launcher.py generic with args passed in

--- neovide_launcher.py
+++ neovide_launcher.py
@@ -2,24 +2,23 @@ import os
 import subprocess
 import uuid
 import time
+import argparse
+import re
 
-def launch_neovide():
+def launch_neovide(args, argv):
     launch_id = str(uuid.uuid4())
     child_env = os.environ.copy()
     child_env["NVIM_LAUNCH_ID"] = launch_id
     child_env["WSLENV"] = "NVIM_LAUNCH_ID/u"
 
-    # TODO: pass all this in from outside
-    proc = subprocess.Popen([r"C:\Program Files\Neovide\neovide.exe", "--wsl", "--neovim-bin", "~/.local/share/bob/nvim-bin/nvim", "--log"],
+    proc = subprocess.Popen([re.sub(r"\$\$nvim", "$nvim", re.sub(r"(?<!\$)\$nvim", args.nvim, x)) for x in argv],
         env=child_env,
-        cwd=r"\\wsl$\Ubuntu\home\windexlight",
+        cwd=args.cwd or None,
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL,
         stdin=subprocess.DEVNULL,
         creationflags=subprocess.DETACHED_PROCESS | subprocess.CREATE_BREAKAWAY_FROM_JOB,
     )
-    print(f"Launched Neovide (PID: {proc.pid}). Waiting for Neovim to initialize...")
-
     handshake_sock = f"/tmp/nvim-handshake-{launch_id}.sock"
     rpc_command = f"'v:lua.ReceiveWindowsPid({proc.pid})'"
 
@@ -28,21 +27,20 @@ def launch_neovide():
         time.sleep(0.2)
         try:
             result = subprocess.run(
-                ["wsl.exe", "nvim", "--headless", "--server", handshake_sock, "--remote-expr", rpc_command],
+                ["wsl.exe", args.nvim, "--headless", "--server", handshake_sock, "--remote-expr", rpc_command],
                 capture_output=True,
                 text=True
             )
             if "HANDSHAKE_COMPLETE" in result.stdout:
-                print(f"Success! Handshake complete. RPC is now bound to /tmp/nvim-win-{proc.pid}.sock")
                 return
-            else:
-                print(result.stdout, result.stderr)
         except Exception as e:
             print(e)
-
-    print("Error: Timed out waiting for Neovim handshake.")
-    pass
+            pass
 
 if __name__ == "__main__":
-    launch_neovide()
-    pass
+    parser = argparse.ArgumentParser(description="Wrapper for launching WSL nvim with QMK Shared Keys Host compatibility")
+    parser.add_argument("--cwd", type=str, default="", help="Windows path to the WSL directory you want nvim to use as its starting cwd. Example: \\\\wsl$\\[distro]\\home\\[username]")
+    parser.add_argument("--nvim", type=str, default="nvim", help="WSL path to nvim binary you wish to use, if you do not have the one you want on the WSL path. May be referenced with $nvim as later argument (use $$nvim for literal $nvim), though note that you will need to escape the $ character, depending on the shell (for example, use `$nvim in PowerShell).")
+    # Remaining args should be the command to launch nvim within WSL, such as "neovide.exe --wsl"
+    args, argv = parser.parse_known_args()
+    launch_neovide(args, argv)
